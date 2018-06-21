8af5fe599de224c065eb1706f03d092a6371b525
Nelson Fonkoua <33722117+nelson888@users.noreply.github.com>
2018-06-21 14:29:34 +0200

    Add files via upload

--- src/model.cpp
+++ src/model.cpp
@@ -53,7 +53,7 @@ void Sphere::render()
 }
 
 
-Face::Face(Vector v1, Vector v2, Point org, double l, double w, Color cl)
+Cube_face::Cube_face(Vector v1, Vector v2, Point org, double l, double w, Color cl)
 {
     vdir1 = 1.0 / v1.norm() * v1;
     vdir2 = 1.0 / v2.norm() * v2;
@@ -64,13 +64,13 @@ Face::Face(Vector v1, Vector v2, Point org, double l, double w, Color cl)
 }
 
 
-void Face::update(double delta_t)
+void Cube_face::update(double delta_t)
 {
-    // Do nothing, no physics associated to a Face
+    // Do nothing, no physics associated to a Cube_face
 }
 
 
-void Face::render()
+void Cube_face::render()
 {
     Point p1 = Point();
     Point p2 = p1, p3, p4 = p1;
